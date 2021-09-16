2bbc8acaeefd62b6d9e8288c601dcfa0ee10931d
analavade <analavade@pluribusdigital.com>
2021-09-16 11:04:37 -0400

    Update artifactory_compliance.sh
    Shell script to check for public repo reference in package config files (Gemfile.lock, package-lock.json, pom.xml)

--- scripts/artifactory_compliance.sh
+++ scripts/artifactory_compliance.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
 
-echo "This repo is in compliance"
+DIR="${1:-.}"
+
+FAILS=0
+
+EXT_RUBY="https://rubygems.org/"
+EXT_NODE="https://registry.npmjs.org/"
+EXT_MAVEN="http://repo1.maven.org/maven2"
+
+## Ruby Check
+while read -r file
+do
+    if grep -q "$EXT_RUBY" "$file"; then
+      echo "$EXT_RUBY in $file";
+      FAILS=1;
+    fi
+done < <(find $DIR -name "Gemfile.lock")
+
+## npm Check
+while read -r file
+do
+    if grep -q "$EXT_NODE" "$file"; then
+      echo "$EXT_NODE in $file";
+      FAILS=1;
+    fi
+done < <(find $DIR -name "package-lock.json")
+
+## maven Check
+while read -r file
+do
+    if grep -q "$EXT_MAVEN" "$file"; then
+      echo "$EXT_MAVEN in $file";
+      FAILS=1;
+    fi
+done < <(find $DIR -name "pom.xml")
+
+if [ $FAILS -eq 1 ]; then
+  echo -e "\nThis repo has files that are not in compliance";
+  exit 1;
+else
+  echo "This repo is in compliance"
+fi
