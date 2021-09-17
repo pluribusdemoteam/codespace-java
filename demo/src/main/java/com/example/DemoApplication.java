19cfbed10521323212b87387d8e23773f151c051
analavade <analavade@pluribusdigital.com>
2021-09-17 15:08:18 -0400

    Update DemoApplication.java
    Triggering codacy issue

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	private static String SECRET_PASSWORD = "letMeIn!";
+
 	public static void main(String[] args) {
+		System.out.println("SECRET_PASSWORD"+ SECRET_PASSWORD);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
