9b0057ff1234463c0f45e55717843c89704c9944
analavade <analavade@pluribusdigital.com>
2021-09-17 13:19:41 -0400

    Update DemoApplication.java

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		String password = "Apple";
+		System.out.println("Password"+ password);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
