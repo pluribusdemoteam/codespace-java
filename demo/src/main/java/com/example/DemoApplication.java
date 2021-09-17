0d30a71650560ec69c4b49215cca2ea0f78447c8
analavade <analavade@pluribusdigital.com>
2021-09-17 15:09:19 -0400

    Update DemoApplication.java

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	private String SECRET_PASSWORD = "letMeIn!";
-
 	public static void main(String[] args) {
-		System.out.println("SECRET_PASSWORD"+ SECRET_PASSWORD);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
