a11996a11f4dc5011567dd984dd69d6f838a13d4
analavade <analavade@pluribusdigital.com>
2021-09-17 10:49:27 -0400

    Update artifactory_compliance.sh

--- scripts/artifactory_compliance.sh
+++ scripts/artifactory_compliance.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm -f NONCOMPLIANT.txt
+rm -f COMPLIANT.txt
+
 DIR="${1:-.}"
 
 FAILS=0
@@ -36,8 +39,10 @@ do
 done < <(find $DIR -name "pom.xml")
 
 if [ $FAILS -eq 1 ]; then
-  echo -e "\nThis repo has files that are not in compliance.";
+  echo -e "\nThis repo has files that are not in compliance";
+  touch NONCOMPLIANT.txt
   exit 1;
 else
-  echo "This repo is in compliance."
+  echo "This repo is in compliance"
+  touch COMPLIANT.txt
 fi
